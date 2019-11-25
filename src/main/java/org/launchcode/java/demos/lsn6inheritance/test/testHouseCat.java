98ef47a1f761bd9f3a42f845a52806a069b10a33
laalaunch <lastembo@gmail.com>
2019-11-24 22:16:50 -0600

    lesson 6 inheritance chapter items

---
+++ src/main/java/org/launchcode/java/demos/lsn6inheritance/test/testHouseCat.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.demos.lsn6inheritance.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.launchcode.java.demos.lsn6inheritance.HouseCat;
+
+
+public class testHouseCat {
+
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+        assertEquals(7, keyboardCat.getWeight(), .001);
+    }
+}
